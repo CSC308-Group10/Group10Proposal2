20902942974f343378de5f65a6471996d7e7464f
dakotagoodman8 <dakota_goodman8@mymail.eku.edu>
2026-04-07 11:42:49 -0400

    Initial Commit

---
+++ Group10Proposal2/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Group10Proposal2
+//
+//  Created by Goodman, Dakota K. on 4/7/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let group10 = ["Dakota", "Greyson", "Alex"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
